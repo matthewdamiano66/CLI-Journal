1d7f4ad07a75cd011480dbb63dfb63c9f9b3d129
Matthew <58225683+matthewdamiano66@users.noreply.github.com>
2026-02-22 06:30:09 -0800

    Initial Skeleton of the program
    Uploading the initial skeleton of the application, to update later with fully defined functions, uploading for outline of progression in program.

---
+++ main.py
@@ -0,0 +1,26 @@
+import datetime
+
+def instance(filter):
+        with open ("journal.txt",'a') as cursor:
+               cursor.read()
+               contents = cursor.read
+        print(contents)
+        entry = input()
+        filter = entry.strip.lower()
+        return(filter)
+
+def remove():
+       print()
+
+def close():
+       print()
+    
+def main():
+        instance()
+
+
+
+
+
+if __name__ == "__main__" ():
+    main()

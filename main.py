ba6cc88d0cfd8f4c341024391b3588b7ad488b4a
Matthew <58225683+matthewdamiano66@users.noreply.github.com>
2026-03-01 11:00:07 -0500

    CLI Journal v1.0
    Refactor instance function to include timestamp and improve file handling. Handled I/O input and output for communication to txt file using hard coded commands.

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
 import datetime
-
-def instance(filter):
-        with open ("journal.txt",'a') as cursor:
-               cursor.read()
-               contents = cursor.read
+import os
+def instance():
+    with open("journal.txt", "a+") as cursor:
+        cursor.seek(0)
+        contents = cursor.read()
         print(contents)
-        entry = input()
-        filter = entry.strip.lower()
-        return(filter)
+
+        entry = input("New entry: ")
+        timestamp = datetime.datetime.now().strftime("%m/%d/%Y %I:%M:%S %p")
+        cursor.write(entry +"\n"+ timestamp+"\n")
+
+        print("Entry saved.")
 
 def remove():
-       print()
+       print("Deleting...")
+       os.remove("journal.txt")
+       print("Journal Removed")       
 
-def close():
-       print()
     
 def main():
-        instance()
+    commands = ("new", "delete", "close")
+
+    while True:
+        user = input("Enter a command (new, delete, close): ").strip().lower()
+
+        if user == "new":
+            instance()
+
+        elif user == "delete":
+            remove()
+
+        elif user == "close":
+            break
+
+        else:
+            print("Invalid command.")
 
 
 
 
 
-if __name__ == "__main__" ():
-    main()
+if __name__ == "__main__" :
+    main()  
